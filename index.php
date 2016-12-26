99bb2d64d3a69845e0156b4f14eafda2538361f8
isnotfalse <vetrofgoscha2017@yandex.ru>
2016-12-26 07:25:40 +0300

    Hello there!  This is my first commit!

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	echo "Hello, I am Ivan<br>";
+	echo "-Hola!, que tal? <br>";
+	echo "-Hola, muy buen, gracias y tu? <br>";
+	echo "-Yo tambien gracias.";
+        
+        echo "I'm in Net Beans"
+?>
